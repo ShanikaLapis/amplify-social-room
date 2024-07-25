098f616d167590d4d9ad6724a34c1f86c5a62fce
Shanika Ediriweera <shanika.ediriweera@lapisit.com.au>
2024-07-26 06:53:09 +1000

    added multi-cursor capability

--- src/RoomSelector.tsx
+++ src/RoomSelector.tsx
@@ -1,6 +1,9 @@
 import { Schema } from "../amplify/data/resource"
 import { defaultRoom } from "./utils";
 import { useEffect, useState } from "react";
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
 
 export function RoomSelector({
   currentRoomId,
@@ -13,7 +16,13 @@ export function RoomSelector({
   const [rooms, setRooms] = useState<Schema["Room"]["type"][]>([defaultRoom])
   
   useEffect(() => {
-    // Add observeQuery code here
+    // set up a live feed inside the useEffect
+    const sub = client.models.Room.observeQuery().subscribe({
+      next: (data) => {
+        setRooms([defaultRoom, ...data.items])
+      }
+    })
+    return () => sub.unsubscribe()
   }, [])
 
   return <>
@@ -23,7 +32,12 @@ export function RoomSelector({
       {rooms.map(room => <option value={room.id} key={room.id}>{room.topic}</option>)}
     </select>
     <button onClick={async () => {
-      // Add create Room logic here
+      const newRoomName = window.prompt("Enter a name for the new room");
+      if (!newRoomName) return;
+      const { data: room } = await client.models.Room.create({ topic: newRoomName });
+      if (room !== null) {
+        onRoomChange(room.id);
+      }
     }}>[+ add]</button>
   </>
 }
